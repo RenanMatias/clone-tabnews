7e53095dfbae18fe78eca8918f6b376fca8b65e0
Renan Matias <rnmatias@hotmail.com>
2023-05-05 00:54:59 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Sofia e Bonita, eu amo vocês de montão. Se vocês me amam, imitam uma arara!</h1>
+  return <h1>Sofia e Bonita, eu amo vocês de montão. Se vocês me amam, imitam uma arara! 🦜</h1>
 }
 
 export default Home;
