c887f55f25e6cfec315cdf2d33a8b72c57769fe2
Renan Matias <rnmatias@hotmail.com>
2023-06-23 11:57:24 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Realização de um projeto do zero com a ajuda do incrível @filipedeschamps!
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Sofia e Bonita, eu amo vocês de montão. Se vocês me amam, imitam uma arara! 🦜</h1>
+  return (
+    <h1>
+      Sofia e Bonita, eu amo vocês de montão. Se vocês me amam, imitam uma
+      arara! 🦜
+    </h1>
+  );
 }
 
 export default Home;
